882b0193f562459fae9c940bec0314d253c7da8e
SgtRoflol <tim.feucht@icloud.com>
2024-07-20 00:08:29 +0200

    Zwischenstand update weil mergen

--- controllers/equipmentController.js
+++ controllers/equipmentController.js
@@ -10,81 +10,110 @@ async function saveFile(file) {
     return filename;
 }
 
-//need to fix: 
+//Holt sich vom Backend alle Equipments
 const getEquipment = async () => {
     return axios.get('http://localhost:3000/equipment')
-        .then(response => {
-        return { "status": 200, "data": response.data };;
-    });
 };
 
-const createEquipment = async (req) => { 
 
-        if (!req.files) {
-            axios.post('http://localhost:3000/equipment/', req.body)
-                .then(response => {
-                    return { "status": 200, "data": response };
-                });
-        } else {
-            let filename = await saveFile(req.files.file);
-            //we require fs to read the file as a stream
-            var fs = require('fs');
-    
-            //we require form-data to send the file and other form fields
-            let FormData = require('form-data');
-            let formData = new FormData();
-    
-            //fs.createReadStream is needed to read the file and get the file size
-            formData.append("file", fs.createReadStream(filename), { knownLength: fs.statSync(filename).size });
-    
-            //append the other form fields
-            formData.append("userid", req.body.userid);
-            formData.append("title", req.body.title);
-            formData.append("articlenumber", req.body.articlenumber);
-            formData.append("description", req.body.description);
-            formData.append("count", req.body.count);
-    
-            //set headers with size of form-data
-            const headers = {
-                ...formData.getHeaders(),
-                "Content-Length": formData.getLengthSync()
-            };
-    
-           return axios.post('http://localhost:3000/equipment', formData, { headers })
-                .then(function (response) {
-                    //delete the file after sending
-                    fs.unlinkSync(filename);
-                    return { "status": 200, "data": response.data };
-                })
-                .catch(function (error) {
-                    res.send(error)
-                });
-        }
+const createEquipment = async (req, res) => {
+
+    if (!req.files) {
+        axios.post('http://localhost:3000/equipment/', req.body)
+            .then(response => {
+                return { "status": 200, "data": response };
+            });
+    } else {
+        let filename = await saveFile(req.files.file);
+        //we require fs to read the file as a stream
+        var fs = require('fs');
+
+        //we require form-data to send the file and other form fields
+        let FormData = require('form-data');
+        let formData = new FormData();
+
+        //fs.createReadStream is needed to read the file and get the file size
+        formData.append("file", fs.createReadStream(filename), { knownLength: fs.statSync(filename).size });
+
+        //append the other form fields
+        formData.append("userid", req.body.userid);
+        formData.append("title", req.body.title);
+        formData.append("articlenumber", req.body.articlenumber);
+        formData.append("description", req.body.description);
+        formData.append("count", req.body.count);
+
+        //set headers with size of form-data
+        const headers = {
+            ...formData.getHeaders(),
+            "Content-Length": formData.getLengthSync()
+        };
+
+        return axios.post('http://localhost:3000/equipment', formData, { headers }) //Doppeltes return -> alles nach return wird nicht ausgeführt
+            .then(function (response) {
+                //delete the file after sending
+                fs.unlinkSync(filename);
+                return { "status": 200, "data": response.data };
+            })
+            .catch(function (error) {
+                res.send(error)
+            });
+    }
 }
 
+//Holt einzelnes Equipment vom Backend
+const getSingleEquipment = (id) => {
+    return axios.get('http://localhost:3000/equipment/' + id);
+}
 
-const getSingleEquipment = async(id) => {
-    return axios.get('http://localhost:3000/equipment/' + id)
+//Löscht einzelne ID vom Backend
+const deleteEquipment = (id) => {
+    return axios.delete('http://localhost:3000/equipment/' + id);
+}
+
+const updateEquipment = async (req, res) => {
+    if (!req.files) {
+        axios.put('http://localhost:3000/equipment/', req.body)
             .then(response => {
-                console.log(response);
-                return {"status": 200, "data": response.data};
+                return { "status": 200, "data": response };
+            });
+    } else {
+        let filename = await saveFile(req.files.file);
+        //we require fs to read the file as a stream
+        var fs = require('fs');
+
+        //we require form-data to send the file and other form fields
+        let FormData = require('form-data');
+        let formData = new FormData();
+
+        //fs.createReadStream is needed to read the file and get the file size
+        formData.append("file", fs.createReadStream(filename), { knownLength: fs.statSync(filename).size });
+
+        //append the other form fields
+        formData.append("userid", req.body.userid);
+        formData.append("title", req.body.title);
+        formData.append("articlenumber", req.body.articlenumber);
+        formData.append("description", req.body.description);
+        formData.append("count", req.body.count);
+
+        //set headers with size of form-data
+        const headers = {
+            ...formData.getHeaders(),
+            "Content-Length": formData.getLengthSync()
+        };
+
+        return axios.put('http://localhost:3000/equipment', formData, { headers }) //Doppeltes return -> alles nach return wird nicht ausgeführt
+            .then(function (response) {
+                //delete the file after sending
+                fs.unlinkSync(filename);
+                return { "status": 200, "data": response.data };
+            })
+            .catch(function (error) {
+                res.send(error)
             });
     }
 
-const deleteEquipment = async(id) => {
-    return axios.delete('http://localhost:3000/equipment/' + id)
-            .then(response => {
-                return {"status": 200, "data": response.data};
-            });
-    }
+}
 
-const updateEquipment = async(req) => { 
-    return axios.put('http://localhost:3000/equipment/' + req.body.id, req.body)
-        .then(response => {
-            return { "status": 200, "data": response.data };
-        });
-    }
-    
 
 module.exports = {
     getEquipment,
--- routes/equipment.js
+++ routes/equipment.js
@@ -4,47 +4,52 @@ let equipmentController = require('../controllers/equipmentController');
 
 //equipment routes
 router.route('/')
-.get(async (req, res, next) => {
-      let response = await equipmentController.getEquipment();
-      res.render('equipment', { "data": response.data, title: 'Ausleihen' });
-})
-.post((req, res, next) => {
-  equipmentController.createEquipment(req)
-    .then(response => {
-      res.redirect('/equipment/' + response.data.id);
-    })
-    .catch(error => {
-      next(error);
-    });
-});
+  .get(async (req, res, next) => {
+    let response = await equipmentController.getEquipment();
+    res.render('equipment', { "data": response.data });
+  })
+  .post((req, res, next) => {
+    equipmentController.createEquipment(req, res)
+      .then(response => {
+        res.redirect('/equipment/' + response.data.id);
+      })
+      .catch(error => {
+        next(error);
+      });
+  });
+//Equipment löschen
+router.route('/delete/:id')
+  .get(async (req, res, nex) => {
+    equipmentController.deleteEquipment(req.params.id)
+      .then(response => {
+        res.redirect('/equipment');
+      })
+      .catch(error => {
+        next(error);
+      });
+  });
 
+router.route('/edit/:id')
+  .get(async (req, res, next) => {
+    let response = await equipmentController.getSingleEquipment(req.params.id);
+    res.render('editEquip', { "data": response.data });
+  })
+  .post((req, res, next) => {
+    equipmentController.updateEquipment(req)
+      .then(response => {
+        res.redirect('/equipment/' + response.data.id);
+      })
+      .catch(error => {
+        next(error);
+      });
+  });
 
 
 router.route('/:id')
-.get(async (req, res, next) => {
-      let response = await equipmentController.getSingleEquipment(req.params.id);
-      res.render('singleEquipSite', { "data": response.data, title: response.data.title });
-}) //delete function is in beta
-.delete(async(req, res, next) => {
-  console.log("deleted");
-  equipmentController.deleteEquipment(req.params.id)
-    .then(response => {
-     console.log('checkpoint');
-     res.render('equipment', { "data": response.data, title: 'Ausleihen' });
-    })
-    .catch(error => {
-      next(error);
-    });
-})
-.put((req, res, next) => {
-  equipmentController.updateEquipment(req)
-    .then(response => {
-      res.redirect('/equipment/' + response.data.id);
-    })
-    .catch(error => {
-      next(error);
-    });
-});
+  .get(async (req, res, next) => {
+    let response = await equipmentController.getSingleEquipment(req.params.id);
+    res.render('singleEquipSite', { "data": response.data });
+  });
 
 
 
